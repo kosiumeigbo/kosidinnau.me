279308cb9df39278512922a9f9359bc9f9cae732
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-03-29 20:21:41 +0000

    feat: import and use geist font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { GeistSans } from "geist/font/sans";
 
 export const metadata: Metadata = {
   title: "Kosidinna Umeigbo",
@@ -15,8 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={GeistSans.variable}>
     </html>
   );
 }
