1f29ad38531a6bbb79bc1edce84b1dc644b4c879
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2024-10-13 19:41:35 +0100

    refactor: change project to Astro project

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <h1 className="text-3xl font-normal text-center flex items-center flex-col justify-between">
-      We&apos;re cooking something...
-    </h1>
-  );
-}
---
+++ prettier.config.js
@@ -0,0 +1,22 @@
+/** @type {import("prettier").Config} */
+
+const config = {
+  arrowParens: "always",
+  bracketSpacing: true,
+  insertPragma: false,
+  singleAttributePerLine: false,
+  bracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 120,
+  quoteProps: "preserve",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  useTabs: false,
+  embeddedLanguageFormatting: "auto",
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindFunctions: ["clsx", "cn", "twMerge"],
+  tailwindAttributes: ["className"],
+};
+
+export default config;
