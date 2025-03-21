599a6d6a4f248b7001985aa3bacebeaf07ad6ec0
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-03-21 21:28:29 +0000

    chore: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,17 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./contents/**/*.html"],
   theme: {
     extend: {
       screens: {
-        xs: "420px"
+        xs: "420px",
       },
     },
     fontFamily: {
-      sans: ["var(--font-geist-sans)"]
-    }
+      sans: ["var(--font-geist-sans)"],
+    },
   },
-  plugins: []
+  plugins: [],
 };
 export default config;
