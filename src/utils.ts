34aebfb765be145320c287589a03fc53a3e88942
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-03-24 23:39:38 +0000

    chore: update blog utils

--- src/utils.ts
+++ src/utils.ts
@@ -9,43 +9,54 @@ const getBlogSlugs = function () {
 };
 
 const getHtmlContents = async function (slug: string, index: number) {
-  try {
-    const pathToFile = path.resolve(process.cwd(), "content", slug, "index.html");
-    const fileStream = fs.createReadStream(pathToFile);
+  const pathToFile = path.resolve(process.cwd(), "content", slug, "index.html");
+  const fileStream = fs.createReadStream(pathToFile);
 
-    const rl = readline.createInterface({
-      input: fileStream,
-      crlfDelay: Infinity,
-    });
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
 
-    const lines: string[] = [];
-    for await (const line of rl) {
-      lines.push(line);
-    }
-
-    let endOfFrontmatter = 0;
-    let linesIndex = 0;
-
-    while (endOfFrontmatter < 2) {
-      if (lines[linesIndex].trim() === "---") {
-        endOfFrontmatter = endOfFrontmatter + 1;
-      }
-      linesIndex++;
-    }
-
-    const frontMatterArray = lines
-      .splice(0, linesIndex)
-      .filter((str) => str.trim() !== "" && str.trim() !== "---")
-      .map((str) => str.trim());
-
-    const htmlContent = lines.join("");
-
-    console.log(`For file ${(index + 1).toString()}: `, frontMatterArray, htmlContent);
-    return { title: "", date: Date.now().toString(), htmlContent };
-  } catch (e) {
-    console.error(`For file ${(index + 1).toString()}: `, e);
-    return "";
+  const lines: string[] = [];
+  for await (const line of rl) {
+    lines.push(line);
   }
+
+  let endOfFrontmatter = 0;
+  let linesIndex = 0;
+
+  while (endOfFrontmatter < 2) {
+    if (lines[linesIndex].trim() === "---") {
+      endOfFrontmatter = endOfFrontmatter + 1;
+    }
+    linesIndex++;
+  }
+
+  const frontMatterArray = lines
+    .splice(0, linesIndex)
+    .filter((str) => str.trim() !== "" && str.trim() !== "---")
+    .map((str) => str.trim());
+
+  const htmlContent = lines.join("");
+
+  console.log(`For file ${(index + 1).toString()}: `, frontMatterArray, htmlContent);
+  return { title: "", date: Date.now().toString(), htmlContent, slug };
 };
 
-console.log(getBlogSlugs().map((slug, i) => getHtmlContents(slug, i)));
+export const getBlogPostInformationForSlug = async function (slug: string) {
+  const promisesArray = getBlogSlugs().map((slug, i) => getHtmlContents(slug, i));
+  const settledValues = await Promise.allSettled(promisesArray);
+  const blogPostObjectsArray = settledValues
+    .filter(
+      (obj): obj is PromiseFulfilledResult<{ title: string; date: string; htmlContent: string; slug: string }> =>
+        obj.status === "fulfilled",
+    )
+    .map((obj) => obj.value);
+  console.log(blogPostObjectsArray);
+  const slugBlogPostObject = blogPostObjectsArray.find((obj) => obj.slug === slug);
+  return slugBlogPostObject;
+};
+
+// void getBlogPostInformationForSlug();
+
+// console.log(getBlogSlugs().map((slug, i) => getHtmlContents(slug, i)));
