073d62c9fb270bca98da83076e97bfb2b268002a
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-04-06 13:55:29 +0100

    chore: create types folder

---
+++ src/lib/types/index.ts
@@ -0,0 +1,6 @@
+export const propKeysStringValue = ["title", "description"] as const;
+export const propKeysNotStringValue = ["date", "tags"] as const;
+// export const allPropKeys = [...propKeysStringValue, ...propKeysNotStringValue];
+
+export type PropKeysStringValueType = (typeof propKeysStringValue)[number];
+export type PropKeysNotStringValueType = (typeof propKeysNotStringValue)[number];
