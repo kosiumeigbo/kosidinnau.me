118b30e7eccc8e854f398ec32e5df1beb6e902a7
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-04-07 23:04:48 +0100

    chore: comment out the 'tags' in the array

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,5 +1,5 @@
 export const propKeysStringValue = ["title", "description"] as const;
-export const propKeysNotStringValue = ["date", "tags"] as const;
+export const propKeysNotStringValue = ["date" /* "tags" */] as const;
 // export const allPropKeys = [...propKeysStringValue, ...propKeysNotStringValue];
 
 export type PropKeysStringValueType = (typeof propKeysStringValue)[number];
