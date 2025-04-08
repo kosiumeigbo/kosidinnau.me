49edb2f4a4fbb821082bb5b1daa4d893d3b00518
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-04-08 09:31:37 +0100

    chore: edit types.ts file

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,6 +1,11 @@
+import * as z from "zod";
+
 export const propKeysStringValue = ["title", "description"] as const;
-export const propKeysNotStringValue = ["date" /* "tags" */] as const;
+export const propKeysNonStringValue = ["date", "tags"] as const;
 // export const allPropKeys = [...propKeysStringValue, ...propKeysNotStringValue];
 
+export const propKeysStringValueSchema = z.enum(propKeysStringValue);
+export const propKeysNonStringValueSchema = z.enum(propKeysNonStringValue);
+
 export type PropKeysStringValueType = (typeof propKeysStringValue)[number];
-export type PropKeysNotStringValueType = (typeof propKeysNotStringValue)[number];
+export type PropKeysNonStringValueType = (typeof propKeysNonStringValue)[number];
