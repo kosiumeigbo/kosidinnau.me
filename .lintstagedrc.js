ad974ad67bdefd6465914a6e4a7c070b14229016
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-04-10 22:16:01 +0100

    chore: update '.lintstagedrc.js'

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,6 @@
-const path = require("path");
-
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`;
+const eslintCommand = (filenames) => `eslint ${filenames.join(" ")}`;
 
 module.exports = {
   "*.{js,jsx,ts,tsx,html,css}": ["prettier --write"],
-  "src/**/*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "src/**/*.{js,jsx,ts,tsx}": [eslintCommand],
 };
