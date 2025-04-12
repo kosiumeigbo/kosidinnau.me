d58fcb48e6bf3705dcb4f58615b7f9c983b8343e
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-04-12 12:40:58 +0100

    chore: turn 'reload-server' script to js file

---
+++ src/lib/scripts/reload-server.js
@@ -0,0 +1,37 @@
+const { config } = require("dotenv");
+const { watch } = require("chokidar");
+const { WebSocketServer } = require("ws");
+const { resolve } = require("node:path");
+const { createServer } = require("node:http");
+
+config({ path: ".env" });
+
+const WS_SERVER_PORT = "9000";
+const HTTP_SERVER_PORT = "8080";
+
+const writingsDirectory = resolve(process.cwd(), "writings");
+console.log(writingsDirectory);
+const watcher = watch(writingsDirectory, {});
+watcher.on("add", (path) => {
+  console.log(`This was added: ${path}`);
+});
+watcher.on("change", (path) => {
+  console.log(`[Reload] File changed: ${path}`);
+});
+
+const wss = new WebSocketServer({ port: parseInt(process.env.WS_SERVER_PORT || WS_SERVER_PORT, 10) });
+wss.on("connection", (ws) => {
+  console.log("[Reload] Client connected");
+  watcher.on("change", (path) => {
+    console.log(`[Reload] File changed: ${path}`);
+    if (ws.readyState === WebSocket.OPEN) {
+      ws.send("reload");
+    }
+  });
+});
+
+/* HTTP Server Running */
+const httpPort = parseInt(process.env.HTTP_SERVER_PORT || HTTP_SERVER_PORT, 10);
+createServer().listen(httpPort, () => {
+  console.log(`HTTP Server running on port ${httpPort.toString()}...`);
+});
