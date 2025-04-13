c1c6cebf01fce917f5144dff32da630ef3c69659
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-04-13 18:36:57 +0100

    style: move the writing css styles to globals.css

--- src/app/writing/[slug]/page.tsx
+++ src/app/writing/[slug]/page.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { getMetaDataForFileInWritings } from "@/lib/writings";
-import style from "./style.module.css";
 import { redirect } from "next/navigation";
 
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
   if (!slug) return redirect("/");
 
-  const post = await getMetaDataForFileInWritings(slug);
-  if (!post) return redirect("/");
+  const writingMetaData = await getMetaDataForFileInWritings(slug);
+  if (!writingMetaData) return redirect("/");
 
-  const { htmlContent } = post;
+  const { htmlContent } = writingMetaData;
 
-  return <div dangerouslySetInnerHTML={{ __html: htmlContent }} className={style.blogEntry}></div>;
+  return (
+    <>
+      <div dangerouslySetInnerHTML={{ __html: htmlContent }} className="writing"></div>
+    </>
+  );
 }
