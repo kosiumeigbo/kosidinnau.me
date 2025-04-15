28f960bc299dc3a3db2636cc24e82465bc4a53af
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-04-15 19:17:32 +0100

    style: update 'writings' route page.tsx

--- src/app/(main)/writings/page.tsx
+++ src/app/(main)/writings/page.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { getMetaDataForAllFilesInWritings } from "@/lib/writings";
+import { Container } from "@/lib/components/layouts/container";
 
 export default async function Page() {
   const writingObjectsArray = await getMetaDataForAllFilesInWritings();
   return (
-    <div>
-      {writingObjectsArray.map((obj, i) => (
-        <div key={i}>
-          {i + 1} {obj.title}
-        </div>
-      ))}
+    <div className="w-full">
+      <Container>
+        {writingObjectsArray.map(({ title, slug, description }, i) => (
+          <div key={i}>
+            {i + 1} {title}
+          </div>
+        ))}
+      </Container>
     </div>
   );
 }
