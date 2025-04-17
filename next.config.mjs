703eaf44a7612ef641852df5fe4c6af7c62546bb
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-04-17 13:16:55 +0100

    build: add some redirects to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { eslint: { dirs: ["src"] }, images: { unoptimized: true } };
+const nextConfig = {
+  eslint: { dirs: ["src"] },
+  images: { unoptimized: true },
+  async redirects() {
+    return [{ source: "/", destination: "/about", permanent: true }];
+  },
+};
 
 export default nextConfig;
