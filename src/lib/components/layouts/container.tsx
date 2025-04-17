f81e51318a03dc8eac6ce7c891468c08dfd04426
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-04-17 13:43:25 +0100

    style: update container.tsx

--- src/lib/components/layouts/container.tsx
+++ src/lib/components/layouts/container.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 export function Container({ className, children, ...props }: ComponentProps<"div">): ReactElement {
   return (
     <div
-      className={cn("mx-auto h-full max-w-screen-2xl px-2 xs:px-2.5 sm:px-3 md:px-4 lg:px-5 xl:px-6", className)}
+      className={cn("mx-auto h-full w-full max-w-screen-2xl px-2 xs:px-2.5 sm:px-3 md:px-4 lg:px-5 xl:px-6", className)}
       {...props}
     >
       {children}
