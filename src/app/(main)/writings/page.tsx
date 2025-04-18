f29e23afc18f2612757e6a72fe87f4485905cace
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-04-18 10:22:32 +0100

    style: basis for '/writings' page

--- src/app/(main)/writings/page.tsx
+++ src/app/(main)/writings/page.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { getMetaDataForAllFilesInWritings } from "@/lib/writings";
 import { Container } from "@/lib/components/layouts/container";
+import Link from "next/link";
 
 export default async function Page() {
   const writingObjectsArray = await getMetaDataForAllFilesInWritings();
   return (
     <div className="w-full">
       <Container>
-        {writingObjectsArray.map(({ title, slug, description }, i) => (
-          <div key={i}>
-            {i + 1} {title}
-          </div>
-        ))}
+        <ol>
+          {writingObjectsArray.map(({ title, slug, description }, i) => (
+            <li key={i}>
+              <div>
+                {i + 1} <Link href={`/writings/${slug}`}>{title}</Link>
+              </div>
+              <p>{description}</p>
+            </li>
+          ))}
+        </ol>
       </Container>
     </div>
   );
