dfc5f964a3b01c247dffd360e826ff7e36aec29c
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-04-19 01:51:08 +0100

    style: update page.tsx for '/writings' route

--- src/app/(main)/writings/page.tsx
+++ src/app/(main)/writings/page.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { getMetaDataForAllFilesInWritings } from "@/lib/writings";
-import { Container } from "@/lib/components/layouts/container";
+import { Container } from "@/lib/components";
 import Link from "next/link";
+import { Metadata } from "next";
+
+export const metadata: Metadata = { title: "Writings | Kosidinna Umeigbo" };
 
 export default async function Page() {
   const writingObjectsArray = await getMetaDataForAllFilesInWritings();
   return (
-    <div className="w-full">
+    <div className="w-full py-5 md:py-9">
       <Container>
-        <ul className="list-outside list-disc">
+        <p className="mb-5 text-sm sm:text-base md:mb-8">Links to all of my public pieces are below:</p>
+        <ul className="">
           {writingObjectsArray
             .sort((a, b) => {
               if (a.date < b.date) {
@@ -20,12 +24,14 @@ export default async function Page() {
               return 0;
             })
             .map(({ title, slug, description }, i) => (
-              <li key={i}>
-                <div className="ml-5">
-                  <div className="italic underline">
+              <li key={i} className="mb-3 sm:mb-5">
+                <div className="grid grid-cols-[auto_1fr] gap-x-2 sm:gap-x-3">
+                  <span>-</span>
+                  <div className="text-sm italic no-underline hover:text-slate-500 sm:text-base md:underline">
                     <Link href={`/writings/${slug}`}>{title}</Link>
                   </div>
-                  <p>{description}</p>
+                  <div></div>
+                  <p className="hidden md:block md:text-sm">{description}</p>
                 </div>
               </li>
             ))}
