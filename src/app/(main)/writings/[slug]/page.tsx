af99591daa04b3d7999b74804cff7c32e15b2e35
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2025-05-18 23:11:27 +0100

    style: add UI for tags

--- src/app/(main)/writings/[slug]/page.tsx
+++ src/app/(main)/writings/[slug]/page.tsx
@@ -24,6 +24,13 @@ export default async function Page({ params }: { params: Promise<{ slug: string 
     <div className="py-5">
       <Container className="max-w-[45rem] text-sm">
         <h1>{title}</h1>
+        <div className="flex flex-wrap gap-1">
+          {tags.map((tag, i) => (
+            <span className="rounded-xl bg-slate-100 px-2 py-1 text-xs sm:text-sm md:text-base" key={i}>
+              {tag}
+            </span>
+          ))}
+        </div>
         <div dangerouslySetInnerHTML={{ __html: htmlContent }} className="w-full" id="writing-piece"></div>
       </Container>
     </div>
