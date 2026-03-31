e743b90a1411fae9942821d60d4504cf070ec5f4
Kosidinna Umeigbo <kosiumeigbo@gmail.com>
2026-03-31 16:36:36 +0100

    chore: test out API fetch call

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -1,7 +1,24 @@
 import { Container } from "@/lib/components";
+import { BadResponse, Book, GoodResponse } from "@/lib/types";
 import React from "react";
 
-export default function Page() {
+export default async function Page() {
+  const res = await fetch(process.env.API_URL, {
+    headers: { "Authorization": `Bearer ${process.env.BEARER_AUTH_TOKEN}`, "Content-Type": "application/json" },
+    method: "GET",
+    next: { revalidate: 0 },
+  });
+
+  if (!res.ok) {
+    const errorResponse = (await res.json()) as BadResponse;
+    console.log(errorResponse.errors);
+    throw new Error(errorResponse.errors[0]);
+  }
+
+  const data = (await res.json()) as GoodResponse<Book>;
+
+  console.log(data.success);
+
   return (
     <Container>
       <h1 className="flex flex-col items-center justify-between text-center text-3xl font-normal">
